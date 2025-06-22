f5c65e60f01bd0b81cd5b29b59d0f06dd8d33c31
Mian Faizan <mfaizanxgp@hotmail.com>
2025-06-22 22:49:08 +0500

    latest

--- src/layout/footer/index.jsx
+++ src/layout/footer/index.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <>
         <div className="w-full min-w-[320px] mt-auto border-t bg-white dark:bg-gray-950 border-gray-200 dark:border-gray-900 px-1.5 sm:px-5 py-5">
             <div className="container max-w-none">
-                <div className="flex items-center justify-between flex-wrap">                    <div className="text-sm text-slate-500 pb-1 sm:pb-0"> &copy; 2025 Benyon Sports. Built by Innovative Labs
+                <div className="flex items-center justify-between flex-wrap">                    <div className="text-sm text-slate-500 pb-1 sm:pb-0"> &copy; 2025 Beynon Sports. Built by Innovative Labs
                     </div>
                     <ul className="flex flex-wrap -mx-3.5 -my-2">
                         <li className="inline-flex">
